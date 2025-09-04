1ebf0ff599f7f86f0697787a5f69cc503fdb3b0b
James Moreau <43487194+JamesMoreau@users.noreply.github.com>
2025-09-04 19:40:07 -0400

    refactor: remove outdated TODO about artwork loading optimization

--- src/main.rs
+++ src/main.rs
@@ -35,8 +35,6 @@ mod visualizer;
 /*
 TODO:
 * Make songs outside of library playable. Add "Open with" from filesystem functionality.
-* Disk and memory usage could be reduced by defering the loading of artwork to when it actually gets displayed
-    instead of storing all of them in memory.
 */
 
 pub const LIBRARY_DIRECTORY_STORAGE_KEY: &str = "library_directory";
