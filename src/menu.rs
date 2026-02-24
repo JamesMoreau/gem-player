f745b0d016cbd6ded22449365e06cb1b93016d0a
James Moreau <43487194+JamesMoreau@users.noreply.github.com>
2026-02-23 21:18:14 -0500

    cleanup

--- src/menu.rs
+++ src/menu.rs
@@ -29,102 +29,87 @@ pub fn create_macos_menu() -> (Menu, Receiver<MenuEvent>) {
         let _ = sender.send(event);
     }));
 
-    let app_menu = Submenu::with_items(
-        "App",
-        true,
-        &[
-            &PredefinedMenuItem::about(None, None),
-            &PredefinedMenuItem::separator(),
-            &PredefinedMenuItem::services(None),
-            &PredefinedMenuItem::separator(),
-            &PredefinedMenuItem::hide(None),
-            &PredefinedMenuItem::hide_others(None),
-            &PredefinedMenuItem::show_all(None),
-            &PredefinedMenuItem::separator(),
-            &PredefinedMenuItem::quit(None),
-        ],
-    )
-    .unwrap();
-
-    let file_menu = Submenu::with_items(
-        "File",
-        true,
-        &[
-            &MenuItem::new("Open with", true, None),
-            &PredefinedMenuItem::separator(),
-            &PredefinedMenuItem::close_window(None),
-        ],
-    )
-    .unwrap();
-
-    let edit_menu = Submenu::with_items(
-        "Edit",
-        true,
-        &[
-            &PredefinedMenuItem::undo(None),
-            &PredefinedMenuItem::redo(None),
-            &PredefinedMenuItem::separator(),
-            &PredefinedMenuItem::cut(None),
-            &PredefinedMenuItem::copy(None),
-            &PredefinedMenuItem::paste(None),
-            &PredefinedMenuItem::select_all(None),
-        ],
-    )
-    .unwrap();
-
-    let view_menu = Submenu::with_items(
-        "View",
-        true,
-        &[
-            &MenuItem::new("Jump to playing track", true, None),
-            &MenuItem::new("Go to library", true, Some(Accelerator::new(Some(Modifiers::META), Code::KeyL))),
-            &MenuItem::new("Go to playlists", true, Some(Accelerator::new(Some(Modifiers::META), Code::KeyP))),
-            &MenuItem::new("Go to settings", true, Some(Accelerator::new(Some(Modifiers::META), Code::Comma))),
-        ],
-    )
-    .unwrap();
-
-    let playback_menu = Submenu::with_items(
-        "Playback",
-        true,
-        &[
-            &MenuItem::new("Play / Pause", true, Some(Accelerator::new(None, Code::Space))),
-            &MenuItem::new("Next track", true, Some(Accelerator::new(Some(Modifiers::META), Code::ArrowRight))),
-            &MenuItem::new(
-                "Previous track",
-                true,
-                Some(Accelerator::new(Some(Modifiers::META), Code::ArrowLeft)),
-            ),
-            &MenuItem::new("Volume up", true, Some(Accelerator::new(Some(Modifiers::META), Code::ArrowUp))),
-            &MenuItem::new("Volume down", true, Some(Accelerator::new(Some(Modifiers::META), Code::ArrowDown))),
-        ],
-    )
-    .unwrap();
-
-    let window_menu = Submenu::with_items(
-        "Window",
-        true,
-        &[
-            &PredefinedMenuItem::minimize(None),
-            &PredefinedMenuItem::maximize(None),
-            &PredefinedMenuItem::separator(),
-            &PredefinedMenuItem::fullscreen(None),
-        ],
-    )
-    .unwrap();
-
-    let help_menu = Submenu::new("Help", true);
-    let report_an_issue = MenuItem::new("Report an issue", true, None);
-    help_menu.append_items(&[&report_an_issue]).unwrap();
-
     let menu = Menu::with_items(&[
-        &app_menu,
-        &file_menu,
-        &edit_menu,
-        &view_menu,
-        &playback_menu,
-        &window_menu,
-        &help_menu,
+        &Submenu::with_items(
+            "App",
+            true,
+            &[
+                &PredefinedMenuItem::about(None, None),
+                &PredefinedMenuItem::separator(),
+                &PredefinedMenuItem::services(None),
+                &PredefinedMenuItem::separator(),
+                &PredefinedMenuItem::hide(None),
+                &PredefinedMenuItem::hide_others(None),
+                &PredefinedMenuItem::show_all(None),
+                &PredefinedMenuItem::separator(),
+                &PredefinedMenuItem::quit(None),
+            ],
+        )
+        .unwrap(),
+        &Submenu::with_items(
+            "File",
+            true,
+            &[
+                &MenuItem::new("Open with", true, None),
+                &PredefinedMenuItem::separator(),
+                &PredefinedMenuItem::close_window(None),
+            ],
+        )
+        .unwrap(),
+        &Submenu::with_items(
+            "Edit",
+            true,
+            &[
+                &PredefinedMenuItem::undo(None),
+                &PredefinedMenuItem::redo(None),
+                &PredefinedMenuItem::separator(),
+                &PredefinedMenuItem::cut(None),
+                &PredefinedMenuItem::copy(None),
+                &PredefinedMenuItem::paste(None),
+                &PredefinedMenuItem::select_all(None),
+            ],
+        )
+        .unwrap(),
+        &Submenu::with_items(
+            "View",
+            true,
+            &[
+                &MenuItem::new("Jump to playing track", true, None),
+                &PredefinedMenuItem::separator(),
+                &MenuItem::new("Go to library", true, Some(Accelerator::new(Some(Modifiers::META), Code::KeyL))),
+                &MenuItem::new("Go to playlists", true, Some(Accelerator::new(Some(Modifiers::META), Code::KeyP))),
+                &MenuItem::new("Go to settings", true, Some(Accelerator::new(Some(Modifiers::META), Code::Comma))),
+            ],
+        )
+        .unwrap(),
+        &Submenu::with_items(
+            "Playback",
+            true,
+            &[
+                &MenuItem::new("Play / Pause", true, Some(Accelerator::new(None, Code::Space))),
+                &MenuItem::new("Next track", true, Some(Accelerator::new(Some(Modifiers::META), Code::ArrowRight))),
+                &MenuItem::new(
+                    "Previous track",
+                    true,
+                    Some(Accelerator::new(Some(Modifiers::META), Code::ArrowLeft)),
+                ),
+                &MenuItem::new("Volume up", true, Some(Accelerator::new(Some(Modifiers::META), Code::ArrowUp))),
+                &MenuItem::new("Volume down", true, Some(Accelerator::new(Some(Modifiers::META), Code::ArrowDown))),
+            ],
+        )
+        .unwrap(),
+        &Submenu::with_items(
+            "Window",
+            true,
+            &[
+                &PredefinedMenuItem::minimize(None),
+                &PredefinedMenuItem::maximize(None),
+                &PredefinedMenuItem::separator(),
+                &PredefinedMenuItem::fullscreen(None),
+            ],
+        )
+        .unwrap(),
+        &Submenu::with_items("Help", true, &[&MenuItem::new("Report an issue", true, None)]).unwrap(),
     ])
     .unwrap();
 
