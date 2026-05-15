e23cfb9ee4f211db4c87eaa7ca87b58b8574ea68
James Moreau <43487194+JamesMoreau@users.noreply.github.com>
2026-05-14 23:48:20 -0400

    clean up widget

--- src/ui/widgets/centered_frame.rs
+++ src/ui/widgets/centered_frame.rs
@@ -1,16 +1,9 @@
 use egui::{Frame, Ui, epaint::MarginF32};
 
 pub fn centered_frame(ui: &mut Ui, add_contents: impl FnOnce(&mut Ui)) {
-    let margin_factor = 1.0 / 4.0;
+    let margin = ui.available_width().min(ui.available_height()) * 0.25;
 
-    Frame::new()
-        .outer_margin(MarginF32::symmetric(
-            ui.available_width() * margin_factor,
-            ui.available_height() * margin_factor,
-        ))
-        .show(ui, |ui| {
-            ui.vertical_centered(|ui| {
-                add_contents(ui);
-            });
-        });
+    Frame::new().outer_margin(MarginF32::same(margin)).show(ui, |ui| {
+        ui.vertical_centered(add_contents);
+    });
 }
