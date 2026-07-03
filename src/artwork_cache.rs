35bbc79e0a4add55088d90c8a0019cae852f7ee9
James Moreau <43487194+JamesMoreau@users.noreply.github.com>
2026-07-03 00:43:31 -0400

    rename

--- src/artwork_cache.rs
+++ src/artwork_cache.rs
@@ -12,14 +12,14 @@ use crate::APP_NAME;
 
 #[fully_pub]
 struct ArtworkCache {
-    dir: PathBuf,
+    directory: PathBuf,
 }
 
 const ARTWORK_CACHE_FILENAME: &str = "playing.png";
 
 impl ArtworkCache {
     pub fn set_playing(&mut self, data: &[u8]) -> io::Result<PathBuf> {
-        let path = self.dir.join(ARTWORK_CACHE_FILENAME);
+        let path = self.directory.join(ARTWORK_CACHE_FILENAME);
         write(&path, data)?;
         Ok(path)
     }
@@ -27,15 +27,15 @@ impl ArtworkCache {
     pub fn new() -> io::Result<Self> {
         let proj_dirs = ProjectDirs::from("", "", APP_NAME).ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "no project dirs"))?;
 
-        let dir = proj_dirs.cache_dir().join("artwork");
+        let directory = proj_dirs.cache_dir().join("artwork");
 
-        create_dir_all(&dir)?;
+        create_dir_all(&directory)?;
 
-        Ok(Self { dir })
+        Ok(Self { directory })
     }
 
     pub fn playing(&self) -> PathBuf {
-        self.dir.join(ARTWORK_CACHE_FILENAME)
+        self.directory.join(ARTWORK_CACHE_FILENAME)
     }
 
     pub fn current_uri(&self) -> String {
