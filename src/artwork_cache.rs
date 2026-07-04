24e171b2440fce309ce99669c95184011ba00772
James Moreau <43487194+JamesMoreau@users.noreply.github.com>
2026-07-04 01:31:30 -0400

    cleanup

--- src/artwork_cache.rs
+++ src/artwork_cache.rs
@@ -1,15 +1,16 @@
 use std::{
-    fs::{create_dir_all, write},
+    fs::{create_dir_all, read_dir, remove_file, write},
     io,
     path::{Path, PathBuf},
 };
 
 use directories::ProjectDirs;
+use lofty::picture::MimeType;
 use m3u::Url;
 
 use crate::APP_NAME;
 
-const ARTWORK_CACHE_FILENAME: &str = "playing.png";
+const ARTWORK_CACHE_FILEBASE: &str = "playing";
 
 pub fn compute_uri(path: &Path) -> String {
     Url::from_file_path(path)
@@ -17,11 +18,26 @@ pub fn compute_uri(path: &Path) -> String {
         .to_string()
 }
 
-pub fn cache_artwork(cache_directory: &Path, data: &[u8]) -> io::Result<String> {
-    let path = cache_directory.join(ARTWORK_CACHE_FILENAME);
+pub fn cache_artwork(cache_directory: &Path, data: &[u8], mime: MimeType) -> io::Result<String> {
+    let path = artwork_cache_path(cache_directory, &mime);
     write(&path, data)?;
 
     Ok(compute_uri(&path))
+}
+
+pub fn artwork_cache_path(cache_directory: &Path, mime: &MimeType) -> PathBuf {
+    let ext = mime.ext().unwrap_or("png");
+    let filename = format!("{}.{}", ARTWORK_CACHE_FILEBASE, ext);
+    
+    cache_directory.join(filename)
+}
+
+pub fn clear_artwork_cache(cache_directory: &Path) -> io::Result<()> {
+    for entry in read_dir(cache_directory)? {
+        remove_file(entry?.path())?;
+    }
+
+    Ok(())
 }
 
 pub fn artwork_cache_dir() -> io::Result<PathBuf> {
